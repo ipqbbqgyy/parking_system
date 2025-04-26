6bc1b9db05fc9acd887010d58b1c9499d5fde80d
ipqbbqgyy <918392049@qq.com>
2025-04-26 15:37:07 +0800

    asdf

--- reboot.sh
+++ reboot.sh
@@ -1,20 +1,17 @@
-echo -e "\033[34m--------------------wsgi process--------------------\033[0m"
+#!/bin/bash
+# reboot_safe.sh
 
-ps -ef|grep five_class_demo_uwsgi.ini | grep -v grep
+echo -e "\033[34m[1/4] 检查运行中的uWSGI进程...\033[0m"
+ps -ef | grep five_class_demo_uwsgi.ini | grep -v grep
 
-sleep 0.5
+echo -e "\n\033[34m[2/4] 优雅重启uWSGI...\033[0m"
+# 使用reload信号（不会断开现有连接）
+pkill -HUP -f five_class_demo_uwsgi.ini
 
-echo -e '\n--------------------going to close--------------------'
+echo -e "\n\033[34m[3/4] 启动新实例...\033[0m"
+# 使用master进程管理模式
+/envs/five_class_demo/bin/uwsgi --ini five_class_demo_uwsgi.ini --daemonize=/var/log/uwsgi_reboot.log
 
-ps -ef |grep five_class_demo_uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
-
-sleep 0.5
-
-echo -e '\n----------check if the kill action is correct----------'
-
-/envs/five_class_demo/bin/uwsgi  --ini five_class_demo_uwsgi.ini &  >/dev/null
-
-echo -e '\n\033[42;1m----------------------started...----------------------\033[0m'
+echo -e "\n\033[42;1m[4/4] 重启完成，验证进程:\033[0m"
 sleep 1
-
-ps -ef |grep five_class_demo_uwsgi.ini | grep -v grep
+ps -ef | grep five_class_demo_uwsgi.ini | grep -v grep
--- stop.sh
+++ stop.sh
@@ -1,11 +1,13 @@
-echo -e "\033[34m--------------------wsgi process--------------------\033[0m"
+#!/bin/bash
+# stop_safe.sh
 
-ps -ef |grep five_class_demo_uwsgi.ini | grep -v grep
+echo -e "\033[34m[1/3] 检查运行进程...\033[0m"
+ps -ef | grep five_class_demo_uwsgi.ini | grep -v grep
 
-sleep 0.5
+echo -e "\n\033[34m[2/3] 优雅停止...\033[0m"
+# 使用SIGTERM信号允许完成当前请求
+pkill -TERM -f five_class_demo_uwsgi.ini
+sleep 3  # 等待请求完成
 
-echo -e '\n--------------------going to close--------------------'
-
-ps -ef |grep five_class_demo_uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
-
-sleep 0.5
+echo -e "\n\033[34m[3/3] 强制清理残留...\033[0m"
+pkill -9 -f five_class_demo_uwsgi.ini 2>/dev/null
